e568ced8fbcb775798fa1ff09458e9154ad32beb
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2023-05-30 12:58:24 +0700

    added contact page and form

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,7 @@
+import ContactForm from "../components/contact/contact-form";
+
 function ContactPage() {
-  return <div>Contact Page</div>;
+  return <ContactForm />
 }
 
 export default ContactPage;
