2f9e7c110e4ab5d42a8fc3ba679efecb4efb5b37
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2023-05-31 23:20:16 +0700

    implement api contact, added head & document file

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,20 @@
+import Head from 'next/head';
 import AllPost from '../../components/posts/all-posts';
 import {getAllPosts} from '../../lib/posts-util';
 
 function AllPostsPage(props) {
-  return <AllPost posts={props.posts} />;
+  return (
+    <>
+      <Head>
+        <title>All Posts</title>
+        <meta
+          name="description"
+          content="A list of all programming-related tutorials and posts!"
+        />
+      </Head>
+      <AllPost posts={props.posts} />
+    </>
+  );
 }
 
 export function getStaticProps() {
